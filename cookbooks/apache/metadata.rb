dd9827c1beebe6484ee8086540bf8c6a06fe96db
root <root@manideep934.mylabserver.com>
2017-07-26 18:14:03 +0000

    updated the metadata

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -1,6 +1,6 @@
 name 'apache'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'rajesh'
+maintainer_email 'rajesh.kollipara.com'
 license 'All Rights Reserved'
 description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
